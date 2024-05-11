8e14eb93858092e9d954bae5895025a52db951c6
hpxix <108882735+hpxix@users.noreply.github.com>
2024-05-11 23:24:09 +0300

    header

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: React.PropsWithChildren ) => {
   return (
     <ThemedLayoutV2
     Header={Header}
-    Title={(titleProps) => <ThemedTitleV2 {...titleProps} text='Refine' /> }
+    Title={(titleProps) => <ThemedTitleV2 {...titleProps} text='Glob' /> }
     >
       {children}
     </ThemedLayoutV2>
