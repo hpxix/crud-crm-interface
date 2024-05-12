0861350cf3eedeb65564cf98f59f0aeb56bf854b
hpxix <108882735+hpxix@users.noreply.github.com>
2024-05-12 23:42:12 +0300

    [Header]

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,11 +1,27 @@
-import CurrentUser from './current-user'
+import CurrentUser from "./current-user";
+import { Layout, Space } from "antd";
 
 const Header = () => {
+
+  const headerStyles: React.CSSProperties = {
+    background: "#FFF",
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: "0, 24px",
+    position: "sticky",
+    top: 0,
+    zIndex: 999,
+  }
+
   return (
     <div>
-      <CurrentUser />
+      <Layout.Header style={headerStyles}>
+        <Space>
+          <CurrentUser />
+        </Space>
+      </Layout.Header>
     </div>
-  )
-}
-export default Header
-
+  );
+};
+export default Header;
