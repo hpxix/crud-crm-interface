58848e7de8dd7939bbba2f7ccc1f2f349cca5fb1
hpxix <108882735+hpxix@users.noreply.github.com>
2024-05-16 13:30:36 +0300

    Deals Chart

--- src/components/Home/deals-charts.tsx
+++ src/components/Home/deals-charts.tsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { Suspense } from "react";
+import { Card, Button } from "antd";
+import { DollarOutlined, RightCircleOutlined } from "@ant-design/icons";
+import { Text } from "../text";
+import { Area, AreaConfig } from "@ant-design/plots";
+import { useList } from "@refinedev/core";
+import { DASHBOARD_DEALS_CHART_QUERY } from "@/graphql/queries";
+import { mapDealsData } from "@/providers/utilities/helpers";
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+import { DashboardDealsChartQuery } from "@/graphql/types";
+import { useNavigation } from "@refinedev/core";
 
 function DealsCharts() {
+  const { list } = useNavigation();
+  const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>({
+    resource: "dealStages",
+    filters: [
+      {
+        field: "title",
+        operator: "in",
+        value: ["WON", "LOST"],
+      },
+    ],
+    meta: {
+      gqlQuery: DASHBOARD_DEALS_CHART_QUERY,
+    },
+  });
+  const dealData = React.useMemo(() => {
+    return mapDealsData(data?.data);
+  }, [data?.data]);
+
+  const config: AreaConfig = {
+    isStack: false,
+    data: dealData,
+    xField: "timeText",
+    yField: "value",
+    seriesField: "state",
+    animation: true,
+    startOnZero: false,
+    smooth: true,
+    legend: {
+      offsetY: -6,
+    },
+    yAxis: {
+      tickCount: 4,
+      label: {
+        formatter: (v) => {
+          return `$${Number(v) / 1000}k`;
+        },
+      },
+    },
+    tooltip: {
+      formatter: (data) => {
+        return {
+          name: data.state,
+          value: `$${Number(data.value) / 1000}k`,
+        };
+      },
+    },
+    areaStyle: (datum) => {
+      const won = "l(270) 0:#ffffff 0.5:#b7eb8f 1:#52c41a";
+      const lost = "l(270) 0:#ffffff 0.5:#f3b7c2 1:#ff4d4f";
+      return { fill: datum.state === "Won" ? won : lost };
+    },
+    color: (datum) => {
+      return datum.state === "Won" ? "  " : "#F5222D";
+    },
+  };
+
   return (
-    <div>DealsCharts</div>
-  )
+    <>
+      <Card
+        style={{ height: "100%" }}
+        headStyle={{ padding: "8px 16px" }}
+        bodyStyle={{ padding: "25px 24px 0 24px" }}
+        title={
+          <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+            <DollarOutlined />
+            <Text size="sm" style={{ marginLeft: "0.5rem" }}>
+              Deals
+            </Text>
+          </div>
+        }
+        extra={
+          <Button onClick={() => list("deals")} icon={<RightCircleOutlined />}>
+            See sales pipeline
+          </Button>
+        }
+      >
+        <Suspense>
+          <Area {...config} height={325} />
+        </Suspense>
+      </Card>
+    </>
+  );
 }
 
-export default DealsCharts
+export default DealsCharts;
