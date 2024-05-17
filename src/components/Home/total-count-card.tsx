2e3355b952179c0ef6815c5671abda4bad5e05e8
hpxix <108882735+hpxix@users.noreply.github.com>
2024-05-17 11:25:55 +0300

    [add] TotalCountCard

--- src/components/Home/total-count-card.tsx
+++ src/components/Home/total-count-card.tsx
@@ -1,5 +1,8 @@
-import { Card } from "antd/lib";
+import { Card, Skeleton } from "antd/lib";
 import React from "react";
+import { totalCountVariants } from "../constants";
+import { Text } from "../text";
+import { Area, AreaConfig } from "@ant-design/plots";
 
 type Props = {
   resources: "companies" | "contacts" | "deals";
@@ -8,6 +11,44 @@ type Props = {
 };
 
 function DashboardTotalCountCard({ resources, isLoading, totalCount }: Props) {
+  const { primaryColor, secondaryColor, icon, title } =
+    totalCountVariants[resources];
+  const config: AreaConfig = {
+    data: totalCountVariants[resources].data,
+    xField: "index",
+    yField: "value",
+    appendPadding: [1, 0, 0, 0],
+    padding: 0,
+    syncViewPadding: true,
+    autoFit: true,
+    tooltip: false,
+    animation: false,
+    xAxis: false,
+    yAxis: {
+      tickCount: 12,
+      label: {
+        style: {
+          stroke: "transparent",
+        },
+      },
+      grid: {
+        line: {
+          style: {
+            stroke: 'transparent'
+          },
+        },
+      },
+    },
+    smooth: true,
+    line: {
+      color: primaryColor, 
+    },
+    areaStyle: () => {
+      return {
+        fill: `l(270) 0:#FFF 0.2${secondaryColor} 1:${primaryColor}`
+      }
+    }
+  };
   return (
     <Card
       style={{ height: "96px", padding: 0 }}
@@ -22,7 +63,31 @@ function DashboardTotalCountCard({ resources, isLoading, totalCount }: Props) {
           whiteSpace: "nowrap",
         }}
       >
-        {icons}
+        {icon}
+        <Text size="md" className="secondary" style={{ marginLeft: "8px" }}>
+          {title}
+        </Text>
+      </div>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Text
+          size="xxxl"
+          strong
+          style={{
+            flex: 1,
+            whiteSpace: "nowrap",
+            flexShrink: 0,
+            textAlign: "start",
+            marginLeft: "48px",
+            fontVariantNumeric: "tabular-nums",
+          }}
+        >
+          {isLoading ? (
+            <Skeleton.Button style={{ marginTop: "8px", width: "74px" }} />
+          ) : (
+            totalCount
+          )}
+        </Text>
+        <Area {...config} style={{ width: "50%", paddingBottom: '10px' }} />
       </div>
     </Card>
   );
