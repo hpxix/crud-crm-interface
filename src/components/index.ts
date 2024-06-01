610c6863849576a0db98b1bddecafdd1428ef64b
hpxix <108882735+hpxix@users.noreply.github.com>
2024-06-01 21:53:59 +0300

    exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -25,3 +25,14 @@ export {
   Text,
   ContactStatusTag,
 };
+export * from "./tags/user-tag";
+export * from "./tasks/form/description";
+export * from "./tasks/form/due-date";
+export * from "./tasks/form/header";
+export * from "./tasks/form/stage";
+export * from "./tasks/form/title";
+export * from "./tasks/form/users";
+
+
+
+
